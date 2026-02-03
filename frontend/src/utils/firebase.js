bfeb97e56c69d3b0af795681e2dba926181f7e9f
rayhanfay <rayhan.al5776@student.unri.ac.id>
2026-02-04 01:14:21 +0700

    fix: firebase config

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -11,5 +11,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
